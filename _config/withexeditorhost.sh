abe863c56b73290f8925dd22c7c06a94296293f7
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2017-03-19 23:46:08 +0900

    Add comment about node vs nodejs

--- _config/withexeditorhost.sh
+++ _config/withexeditorhost.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Fill in the path of the index.js file of the withExEditorHost.
+# Replace "node" command to "nodejs" according to your environment.
 node /path/to/withexeditorhost/index.js
