c8b93a05c82d73784fecd4f4517c9a31aa4691b3
asamuzaK (Kazz) <kazz@asamuzak.jp>
2017-07-21 17:05:14 +0900

    Revert "Refactor constants"
    This reverts commit 6fce1921011713c861ab8fdbf977bf94e8703d53.

--- modules/constant.js
+++ modules/constant.js
@@ -4,49 +4,93 @@
 "use strict";
 {
   /* shared constants */
+  const CONTENT_GET = "getContent";
+  const CONTEXT_MENU = "contextMenu";
+  const CONTEXT_MODE = "contextMode";
+  const EDITOR_CMD_ARGS = "editorCmdArgs";
+  const EDITOR_CONFIG = "editorConfigPath";
+  const EDITOR_CONFIG_FILE = "editorconfig.json";
+  const EDITOR_CONFIG_GET = "getEditorConfig";
+  const EDITOR_CONFIG_RES = "resEditorConfig";
+  const EDITOR_CONFIG_SET = "setEditorConfig";
+  const EDITOR_CONFIG_TS = "editorConfigTimestamp";
+  const EDITOR_FILE_NAME = "editorFileName";
+  const EDITOR_FILE_POS = "editorFileAfterCmdArgs";
+  const EDITOR_LABEL = "editorLabel";
+  const EDITOR_PATH = "editorPath";
+  const EXT_CHROME_ID = "chrome-extension://koghhpkkcndhhclklnnnhcpkkplfkgoi/";
+  const EXT_WEB_ID = "jid1-WiAigu4HIo0Tag@jetpack";
+  const FILE_EXT = "fileExt";
+  const IS_ENABLED = "isEnabled";
+  const KEY_ACCESS = "accessKey";
+  const KEY_EDITOR = "editorShortCut";
+  const KEY_OPTIONS = "optionsShortCut";
+  const HOST = "withexeditorhost";
+  const HOST_DESC = "Native messaging host for withExEditor";
+  const LABEL = "withExEditor";
+  const LOCAL_FILE_VIEW = "viewLocalFile";
+  const MODE_EDIT = "modeEditText";
+  const MODE_MATHML = "modeViewMathML";
+  const MODE_SELECTION = "modeViewSelection";
+  const MODE_SOURCE = "modeViewSource";
+  const MODE_SVG = "modeViewSVG";
+  const ONLY_EDITABLE = "enableOnlyEditable";
+  const OPTIONS_OPEN = "openOptions";
+  const PROCESS_CHILD = "childProcess";
+  const STORAGE_SET = "setStorage";
+  const TEXT_SYNC = "syncText";
+  const TMP_FILES = "tmpFiles";
+  const TMP_FILES_PB = "tmpFilesPb";
+  const TMP_FILES_PB_REMOVE = "removePrivateTmpFiles";
+  const TMP_FILE_CREATE = "createTmpFile";
+  const TMP_FILE_DATA_PORT = "portTmpFileData";
+  const TMP_FILE_GET = "getTmpFile";
+  const TMP_FILE_RES = "resTmpFile";
+  const VARS_SET = "setVars";
+
   module.exports = {
-    CONTENT_GET: "getContent",
-    CONTEXT_MENU: "contextMenu",
-    CONTEXT_MODE: "contextMode",
-    EDITOR_CMD_ARGS: "editorCmdArgs",
-    EDITOR_CONFIG: "editorConfigPath",
-    EDITOR_CONFIG_FILE: "editorconfig.json",
-    EDITOR_CONFIG_GET: "getEditorConfig",
-    EDITOR_CONFIG_RES: "resEditorConfig",
-    EDITOR_CONFIG_SET: "setEditorConfig",
-    EDITOR_CONFIG_TS: "editorConfigTimestamp",
-    EDITOR_FILE_NAME: "editorFileName",
-    EDITOR_FILE_POS: "editorFileAfterCmdArgs",
-    EDITOR_LABEL: "editorLabel",
-    EDITOR_PATH: "editorPath",
-    EXT_CHROME_ID: "chrome-extension://koghhpkkcndhhclklnnnhcpkkplfkgoi/",
-    EXT_WEB_ID: "jid1-WiAigu4HIo0Tag@jetpack",
-    FILE_EXT: "fileExt",
-    IS_ENABLED: "isEnabled",
-    KEY_ACCESS: "accessKey",
-    KEY_EDITOR: "editorShortCut",
-    KEY_OPTIONS: "optionsShortCut",
-    HOST: "withexeditorhost",
-    HOST_DESC: "Native messaging host for withExEditor",
-    LABEL: "withExEditor",
-    LOCAL_FILE_VIEW: "viewLocalFile",
-    MODE_EDIT: "modeEditText",
-    MODE_MATHML: "modeViewMathML",
-    MODE_SELECTION: "modeViewSelection",
-    MODE_SOURCE: "modeViewSource",
-    MODE_SVG: "modeViewSVG",
-    ONLY_EDITABLE: "enableOnlyEditable",
-    OPTIONS_OPEN: "openOptions",
-    PROCESS_CHILD: "childProcess",
-    STORAGE_SET: "setStorage",
-    TEXT_SYNC: "syncText",
-    TMP_FILES: "tmpFiles",
-    TMP_FILES_PB: "tmpFilesPb",
-    TMP_FILES_PB_REMOVE: "removePrivateTmpFiles",
-    TMP_FILE_CREATE: "createTmpFile",
-    TMP_FILE_DATA_PORT: "portTmpFileData",
-    TMP_FILE_GET: "getTmpFile",
-    TMP_FILE_RES: "resTmpFile",
-    VARS_SET: "setVars",
+    CONTENT_GET,
+    CONTEXT_MENU,
+    CONTEXT_MODE,
+    EDITOR_CMD_ARGS,
+    EDITOR_CONFIG,
+    EDITOR_CONFIG_FILE,
+    EDITOR_CONFIG_GET,
+    EDITOR_CONFIG_RES,
+    EDITOR_CONFIG_SET,
+    EDITOR_CONFIG_TS,
+    EDITOR_FILE_NAME,
+    EDITOR_FILE_POS,
+    EDITOR_LABEL,
+    EDITOR_PATH,
+    EXT_CHROME_ID,
+    EXT_WEB_ID,
+    FILE_EXT,
+    IS_ENABLED,
+    KEY_ACCESS,
+    KEY_EDITOR,
+    KEY_OPTIONS,
+    HOST,
+    HOST_DESC,
+    LABEL,
+    LOCAL_FILE_VIEW,
+    MODE_EDIT,
+    MODE_MATHML,
+    MODE_SELECTION,
+    MODE_SOURCE,
+    MODE_SVG,
+    ONLY_EDITABLE,
+    OPTIONS_OPEN,
+    PROCESS_CHILD,
+    STORAGE_SET,
+    TEXT_SYNC,
+    TMP_FILES,
+    TMP_FILES_PB,
+    TMP_FILES_PB_REMOVE,
+    TMP_FILE_CREATE,
+    TMP_FILE_DATA_PORT,
+    TMP_FILE_GET,
+    TMP_FILE_RES,
+    VARS_SET,
   };
 }
