dcf6d91ca7fa4d1b9e6288ca236113d2d52875d8
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-03-02 22:08:07 +0900

    Remove unused samples

--- _config/withexeditorhost.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-:: Fill in the path of the index.js file of the withExEditorHost.
-node "C:\Users\XXX\withExEditorHost\index.js"
--- _config/withexeditorhost.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Fill in the path of the index.js file of the withExEditorHost.
-# Replace "node" command to "nodejs" according to your environment.
-node /path/to/withexeditorhost/index.js
