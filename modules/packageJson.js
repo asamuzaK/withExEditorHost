0b57c3befa28297d5d860192de4db5cc7ba1aa44
asamuzaK (Kazz) <kazz@asamuzak.jp>
2021-10-02 15:43:19 +0900

    Fix packageJson.js

--- modules/packageJson.js
+++ modules/packageJson.js
@@ -16,8 +16,13 @@ const CHAR = 'utf8';
  * @returns {object} - parsed json
  */
 export const parsePackageJson = () => {
-  const dirname = path.dirname(fileURLToPath(import.meta.url));
-  const pkgPath = path.resolve(dirname, '../package.json');
+  const { dir, name } = path.parse(fileURLToPath(import.meta.url));
+  let pkgPath;
+  if (name === 'index') {
+    pkgPath = path.resolve(dir, 'package.json');
+  } else {
+    pkgPath = path.resolve(dir, '../package.json');
+  }
   const file = fs.readFileSync(pkgPath, {
     encoding: CHAR, flag: 'r'
   });
--- test/packageJson.test.js
+++ test/packageJson.test.js
@@ -1,6 +1,9 @@
 /* api */
 import { assert } from 'chai';
 import { describe, it } from 'mocha';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import sinon from 'sinon';
 
 /* test */
 import { parsePackageJson } from '../modules/packageJson.js';
@@ -11,5 +14,18 @@ describe('parsePackageJson', () => {
     assert.isObject(res);
     assert.isDefined(res.name);
     assert.isDefined(res.version);
+  });
+
+  it('should get object', async () => {
+    const dirname = path.resolve(fileURLToPath(import.meta.url), '../../');
+    const stubParse = sinon.stub(path, 'parse').returns({
+      dir: dirname,
+      name: 'index'
+    });
+    const res = await parsePackageJson();
+    assert.isObject(res);
+    assert.isDefined(res.name);
+    assert.isDefined(res.version);
+    stubParse.restore();
   });
 });
