a65b7fcecd1ee96c59d4ae0171633ba2c890545a
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-02-04 17:45:50 +0900

    Update commander

--- modules/commander.js
+++ modules/commander.js
@@ -25,15 +25,17 @@ export const parseCommand = args => {
   if (Array.isArray(args) && args.some(arg => reg.test(arg))) {
     commander.exitOverride();
     commander.version(hostVersion, '-v, --version');
-    commander.command(CMD_SETUP).alias(CMD_SETUP_ALIAS)
-      .description(CMD_SETUP_DESC)
-      .option(CMD_BROWSER, CMD_BROWSER_DESC)
-      .option(CMD_CONFIG_PATH, CMD_CONFIG_PATH_DESC)
-      .option(CMD_OVERWRITE_CONFIG, CMD_OVERWRITE_CONFIG_DESC)
-      .option(CMD_OVERWRITE_EDITOR_CONFIG, CMD_OVERWRITE_EDITOR_CONFIG_DESC)
-      .option(CMD_EDITOR_PATH, CMD_EDITOR_PATH_DESC)
-      .option(CMD_EDITOR_ARGS, CMD_EDITOR_ARGS_DESC)
-      .action(runSetup);
+    if (args.includes('setup') || args.includes('s')) {
+      commander.command(CMD_SETUP).alias(CMD_SETUP_ALIAS)
+        .description(CMD_SETUP_DESC)
+        .option(CMD_BROWSER, CMD_BROWSER_DESC)
+        .option(CMD_CONFIG_PATH, CMD_CONFIG_PATH_DESC)
+        .option(CMD_OVERWRITE_CONFIG, CMD_OVERWRITE_CONFIG_DESC)
+        .option(CMD_OVERWRITE_EDITOR_CONFIG, CMD_OVERWRITE_EDITOR_CONFIG_DESC)
+        .option(CMD_EDITOR_PATH, CMD_EDITOR_PATH_DESC)
+        .option(CMD_EDITOR_ARGS, CMD_EDITOR_ARGS_DESC)
+        .action(runSetup);
+    }
     try {
       commander.parse(args);
     } catch (e) {
